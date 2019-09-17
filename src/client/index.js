85cfcff8b5b7dc8603c6ab1b3d347cc77bf51c39
minung.han <hmu332233@gmail.com>
2019-09-17 23:33:43 +0900

    build: reducer 추가

--- src/client/index.js
+++ src/client/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 
 import 'antd/dist/antd.css';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
 import MainApp from './apps/MainApp';
 
 ReactDOM.render(
-  <MainApp />,
+  <Provider store={store}>
+    <MainApp />
+  </Provider>,
   document.getElementById('app')
 );
