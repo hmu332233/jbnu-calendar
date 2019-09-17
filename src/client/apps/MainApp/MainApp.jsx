e7f3b97c45962bb43dd387b485f58d55bd3fe717
minung.han <hmu332233@gmail.com>
2019-09-17 23:27:38 +0900

    build: 기본 뼈대 추가

---
+++ src/client/apps/MainApp/MainApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MainApp(props) {
+  return (
+    <div>
+      캘린더 시작!
+    </div>
+  );
+}
+
+MainApp.propTypes = {
+};
+MainApp.defaultProps = {
+};
+
+export default MainApp;
---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'antd/dist/antd.css';
+
+import MainApp from './apps/MainApp';
+
+ReactDOM.render(
+  <MainApp />,
+  document.getElementById('app')
+);
