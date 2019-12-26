8c7263b29429e07ba7c7c8016541052ddb8b4097
minung.han <hmu332233@gmail.com>
2019-12-26 18:58:43 +0900

    feat(server): 불필요한 파일 삭제 및 schedule model 추가

---
+++ src/server/models/schedules/model.js
@@ -0,0 +1,3 @@
+const { db, CONSTANTS } = require('./info');
+const dayjs = require('dayjs');
+
--- src/server/routes/api/events.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require('../../controllers');
-
-router.get('/', controller.events.getEventsWithin3Month);
-router.post('/', controller.events.create);
-
-module.exports = router;
