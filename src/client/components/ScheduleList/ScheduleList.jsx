de9e41e3d5cde63ee4b9f18dbd3195c33f7f065f
minung.han <hmu332233@gmail.com>
2020-02-02 18:48:00 +0900

    feat(client): ScheduleListItem component 추가

--- src/client/components/ScheduleList/ScheduleList.jsx
+++ src/client/components/ScheduleList/ScheduleList.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ScheduleList.scss';
 
-import { List, Avatar, Icon } from 'antd';
+import { List } from 'antd';
 
-const IconText = ({ type, text }) => (
-  <span>
-    <Icon type={type} style={{ marginRight: 8 }} />
-    {text}
-  </span>
-);
+import ScheduleListItem from 'components/ScheduleListItem';
 
 function ScheduleList(props) {
   return (
@@ -23,12 +18,7 @@ function ScheduleList(props) {
       }}
       bordered
       dataSource={props.items}
-      renderItem={item => (
-        <List.Item key={item.title} actions={[<IconText type="edit" text="수정" key="list-edit" />, <IconText type="delete" text="삭제" key="list-delete" />]}>
-          <List.Item.Meta title={item.title} description={item.category === 'time' ? `${item.end}` : `${item.start} - ${item.end}`} />
-          {item.body}
-        </List.Item>
-      )}
+      renderItem={item => <ScheduleListItem key={item._id} title={item.title} description={item.category === 'time' ? `${item.end}` : `${item.start} - ${item.end}`} content={item.body} />}
     />
   );
 }
---
+++ src/client/components/ScheduleListItem/ScheduleListItem.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ScheduleListItem.scss';
+
+import { List, Switch, Icon } from 'antd';
+
+const IconText = ({ type, text, onClick }) => (
+  <span onClick={onClick}>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
+
+function ScheduleListItem(props) {
+  return (
+    <List.Item
+      actions={[
+        <Switch defaultChecked size="small" onChange={props.onSwitchChange} />,
+        <IconText type="edit" text="수정" key="list-edit" onClick={props.onEditClick} />,
+        <IconText type="delete" text="삭제" key="list-delete" onClick={props.onDeleteClick} />,
+      ]}
+    >
+      <List.Item.Meta title={props.title} description={props.description} />
+      {props.content}
+    </List.Item>
+  );
+}
+
+ScheduleListItem.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  content: PropTypes.any,
+  onSwitchChange: PropTypes.func,
+  onEditClick: PropTypes.func,
+  onDeleteClick: PropTypes.func,
+};
+ScheduleListItem.defaultProps = {
+  onSwitchChange: v => v,
+  onEditClick: v => v,
+  onDeleteClick: v => v,
+};
+
+export default ScheduleListItem;
