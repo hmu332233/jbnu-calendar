30ca3eb9e27c9b1da8ef64f1d16e36fbb301de47
minung.han <hmu332233@gmail.com>
2020-02-06 03:05:09 +0900

    feat(client): about 페이지 추가

--- src/client/apps/MainApp/MainApp.jsx
+++ src/client/apps/MainApp/MainApp.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const Main = lazy(() => import('pages/Main'));
+const About = lazy(() => import('pages/About'));
 const ScheduleRegister = lazy(() => import('pages/ScheduleRegister'));
 const ScheduleRequest = lazy(() => import('pages/ScheduleRequest'));
 const SignIn = lazy(() => import('pages/SignIn'));
@@ -14,6 +15,7 @@ function MainApp(props) {
       <Suspense fallback={''}>
         <Switch>
           <Route path="/" exact component={Main} />
+          <Route path="/about" exact component={About} />
           <Route path="/request" exact component={ScheduleRequest} />
           <Route path="/schedules" exact component={ScheduleRegister} />
           <Route path="/signin" exact component={SignIn} />
