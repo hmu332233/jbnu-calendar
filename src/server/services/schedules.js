0c79e7da14dd4131ae48439df4766967fcad84d8
minung.han <hmu332233@gmail.com>
2020-02-18 01:34:34 +0900

    feat(server): 스케쥴 등록 요청시 slack 알람 추가

--- src/server/services/schedules.js
+++ src/server/services/schedules.js
@@ -1,7 +1,19 @@
 const Schedules = require('../models/schedules');
+const { sendRequestMessage } = require('../utils/slack');
 
 exports.create = async ({ calendarId, title, body, location, url, start, end, category }) => {
   const newSchedule = await Schedules.create({
+    calendarId,
+    title,
+    body,
+    location,
+    url,
+    start,
+    end,
+    category,
+  });
+
+  sendRequestMessage({
     calendarId,
     title,
     body,
